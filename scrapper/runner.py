68998d9c88de84f24c746e9dd09ea0acae40640c
Robiul <robiul@ajency.in>
2017-09-22 11:20:05 +0530

    created fresh scrapy project

--- scrapper/runner.py
+++ scrapper/runner.py
@@ -4,22 +4,20 @@ from dispatcher import dispatch
 
 if __name__ == "__main__":
 
-    if(len(sys.argv) is 5):
-        marketplace = sys.argv[1]
-        items = sys.argv[2]
-        group_id = sys.argv[3]
-        batch_id = sys.argv[4]
-        products = {
-            marketplace:items
-        }
-        payload = {'products':products,'group_id':group_id,'batch_id':batch_id}
-    else:
-        products = {
-            'amazon':"240#B06ZXSRSZ4,241#B01NATTJ64,242#B01NCV1KH2,243#B01H3540MI,244#B01KVDHYGI"
-        }
+    hotels = [
+        {
+        'name':"vivanta goa",
+        'roomType': "7"
+        },
+        # {
+        # 'name':"ginger hotel goa",
+        # 'roomType': "1"
+        # },
+        # {
+        # 'name':"crown goa",
+        # 'roomType': "7"
+        # }
+    ]
 
-        group_id = 15038979902192
-        batch_id = 80428880224062
-
-        payload = {'products':products,'group_id':group_id,'batch_id':batch_id}
+    payload = {'hotels':hotels}
     dispatch(payload,{})
