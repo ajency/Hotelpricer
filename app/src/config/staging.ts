c1f53bf8bc8c8e41fa7322e14fc9b8a6c67baace
Robiul <robiul@ajency.in>
2017-10-05 11:51:51 +0530

    staging url added

--- app/src/config/staging.ts
+++ app/src/config/staging.ts
@@ -1,5 +1,5 @@
 export const stagingVariables = {
-  repricerApi: 'http://repricer.staging.browntape.com/api',
+  repricerApi: 'http://hotelpricer.ajency.in/api',
   fileStackKey: 'AUhdopW-eT8Gw1JRw3tnXz',
   environmentName: 'Staging Environment',
   ionicEnvName: 'staging'
